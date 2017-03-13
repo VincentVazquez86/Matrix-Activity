b025831c5ae0550de9b1e94830d726efba8f4560
VincentVazquez86 <7vvazquez@stpatrick.org>
2017-03-13 10:53:57 -0500

    Add files via upload

---
+++ power.js
@@ -0,0 +1,17 @@
+const Matrix = require("matrix-js");
+
+let A = Matrix([
+    [0,1,1,1],
+    [1,0,1,0],
+    [1,1,0,0],
+    [1,0,0,0]
+]);
+k
+function power(mtrx,p){
+    //mtrx is the matrix you input
+    //p is the power of the matrix you want
+}
+
+
+//testing code
+console.log(power(A,10));
