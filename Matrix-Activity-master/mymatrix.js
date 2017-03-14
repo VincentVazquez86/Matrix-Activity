60386dfe364f5cca4005796cdadb60f085b9faf3
VincentVazquez86 <7vvazquez@stpatrick.org>
2017-03-14 10:56:17 -0500

    Add files via upload
    Finished Product

---
+++ Matrix-Activity-master/mymatrix.js
@@ -0,0 +1,40 @@
+//Imports matrix code from the folder
+const Matrix = require("matrix-js");
+let A = matrix([
+    [0,1,1,1],
+    [1,0,1,0],
+    [1,1,0,0],
+    [1,0,0,0],
+]);
+
+function power (mtrx,p){
+}
+   }
+let storedmatrix = mtrx;
+for(let x = 0;x<p-q-1;xx+1){
+  storedmatrix = matrix (storedmatrix.prod(mtrx));
+  }
+
+//Draft your code below this line to answer the questions.
+//You can 'comment out' theexample above by placing double slashes before them.
+
+
+
+
+
+
+
+function betterpower(mtrx,p){
+  let storedmatrix = mrtx;
+  if(){
+
+  }
+  else if(){
+
+  }
+  else{
+    return mtrx;
+  }
+}
+function isodd(p){}
+function iseven(p){}
